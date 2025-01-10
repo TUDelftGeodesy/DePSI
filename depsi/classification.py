cf6c4f6bb9ffa396988d34d92eaca09d2a858a9c
Wietske Brouwer <113337632+wietskebrouwer@users.noreply.github.com>
2025-01-10 16:33:34 +0100

    Update depsi/classification.py
    Co-authored-by: Francesco Nattino <49899980+fnattino@users.noreply.github.com>

--- depsi/classification.py
+++ depsi/classification.py
@@ -449,7 +449,7 @@ def _compute_dd_for_correlation(complex_p1, complex_p2):
 
     Parameters
     ----------
-    complex_p1 : (np.ndarray)
+    complex_p1 : np.ndarray
       A complex-valued array representing the first time series.
     complex_p2 : (np.ndarray)
       A complex-valued array representing the second time series.
