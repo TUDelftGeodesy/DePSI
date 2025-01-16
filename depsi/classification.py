f4993c69fccc9300f7a72e37fc5ce04178406e17
Wietske Brouwer <w.s.brouwer@tudelft.nl>
2025-01-16 16:55:28 +0100

    Changed the way dask is imported

--- depsi/classification.py
+++ depsi/classification.py
@@ -2,7 +2,7 @@
 
 from typing import Literal
 
-import dask as da
+import dask.array as da
 import numpy as np
 import xarray as xr
 from scipy.spatial import KDTree
@@ -383,7 +383,7 @@ def detect_side_lobes(stm: xr.Dataset, max_pixel_dist: float, min_correlation: f
         potential_side_lobe_idx = np.union1d(idx_range, idx_azimuth)
         potential_side_lobe_idx = (
             potential_side_lobe_idx.compute()
-            if isinstance(potential_side_lobe_idx, da.array.Array)
+            if isinstance(potential_side_lobe_idx, da.Array)
             else potential_side_lobe_idx
         )
 
