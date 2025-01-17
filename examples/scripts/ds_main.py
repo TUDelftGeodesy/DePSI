65baf333ff4752d005a64c6cc3b947ead189e3f7
yustisiardhitasari <yustisiardhitasari@gmail.com>
2025-01-17 14:45:15 +0700

    master to mother

--- examples/scripts/ds_main.py
+++ examples/scripts/ds_main.py
@@ -51,12 +51,14 @@ def main():
             nlines = stack_meta["nlines"],
             npixels = stack_meta["npixels"],
             slc_dates = stack_meta["slc_dates"],
-            master_date = stack_meta["master_date"],
+            mother_date = stack_meta["mother_date"],
             ds_min_cells = settings["ds_min_cells"],
             path_to_shapefile = settings["parcel_shapefile"],
             path_to_stm = settings["stm_dir"],
             path_to_pe = settings["phase_est_dir"],
         )
+        fileout = os.path.join(settings['stm_dir'], "ds_stm_" + stack_meta['stack_id'] + ".zarr")
+        ds_stm.to_zarr(fileout, mode="w")
 
         ## Merge ps and ds stm
         psds_stm = xr.concat([ps_stm, ds_stm], dim='space')
