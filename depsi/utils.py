4acb2b9dd6adda5a529cf831d74c9b8c9331706c
Simon-van-Diepen <44979564+Simon-van-Diepen@users.noreply.github.com>
2025-01-20 11:10:55 +0100

    Update warning in depsi/utils.py
    Co-authored-by: Ou Ku <o.ku@esciencecenter.nl>

--- depsi/utils.py
+++ depsi/utils.py
@@ -6,7 +6,14 @@ except ImportError:  # UTC can only be imported from Python 3.11 onwards
     from datetime import datetime, timezone
 
     UTC = timezone.utc
-    DeprecationWarning("UTC import is only supported from Python 3.11 onwards. Please upgrade your system.")
+    warnings.warn(
+    """
+    DePSI uses datetime.UTC is only supported from Python 3.11 onwards.
+    For an older Python versions, datetime.timezone.utc is used. 
+    This might be deprecated in newer DePSI versions.
+    """,
+    DeprecationWarning,
+    )
 
 import geopandas
 import numpy as np
