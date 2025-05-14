e9ac9f3b2f51767c7db91b0b1611067e5e117abc
SarahAlidoost <f.alidoost@esciencecenter.nl>
2025-05-14 10:49:59 +0200

    fix minor things

--- examples/scripts/experiments/generate_stm_nl_amsterdam_extended_s1_asc_t088.py
+++ examples/scripts/experiments/generate_stm_nl_amsterdam_extended_s1_asc_t088.py
@@ -1,8 +1,4 @@
 from depsi.config import ConfigGenerateSTM
-import socket
-from dask.distributed import Client
-from dask_jobqueue import SLURMCluster
-
 from depsi.io import read_slc_stack
 from depsi.utils import add_stm_time_deltas, project_stm_coordinates, \
     stm_compute_single_time_differences
@@ -15,34 +11,6 @@ config_path = "./generate_stm_extended_s1_asc_t088.yml"
 
 # Read the config file
 cfg = ConfigGenerateSTM.from_yaml(config_path)
-print(f"Using config file {config_path}.")
-
-## FUNCTIONALITY
-# Start cluster
-
-def get_free_port():
-    """Get a non-occupied port number."""
-    sock = socket.socket()
-    sock.bind(("", 0))  # Bind a port, it will be busy now
-    freesock = sock.getsockname()[1]  # get the port number
-    sock.close()  # Free the port, so it can be used later
-    return freesock
-
-N_WORKERS = 10 # Manual input: number of workers to spin-up
-FREE_SOCKET = get_free_port() # Get a free port
-cluster = SLURMCluster(
-    name="dask-worker",  # Name of the Slurm job
-    queue="normal", # Name of the node partition on your SLURM system
-    cores=4, # Number of cores per worker
-    memory="30 GB",  # Total amount of memory per worker
-    processes=1,  # Number of Python processes per worker
-    walltime="4-00:00:00",  # Reserve each worker for X hour
-    scheduler_options={"dashboard_address": f":{FREE_SOCKET}"},  # Host Dashboard in a free socket
-)
-
-cluster.scale(jobs=N_WORKERS)
-client = Client(cluster)
-
 
 # ############ LOAD THE SLCS FROM ZARR ###############
 slcs = read_slc_stack(cfg.paths['slc_file'])
@@ -64,7 +32,7 @@ stm = stm_add_incremental_recal_nad_nmad(
     method=cfg.stm_add_incremental.method,
     recalibration_jump_size=cfg.stm_add_incremental.recalibration_jump_size,
 )
-print("Added incremental/recal NAD/NMAD")
+print(f"Added incremental/recal {cfg.stm_add_incremental.method}")
 
 # Add RD coordinates to the STM
 stm = project_stm_coordinates(stm, cfg.project_stm_coordinates_projection)
@@ -98,7 +66,6 @@ if cfg.do_stm_partitioning:
     partitioning_single_difference = cfg.stm_partitioning_single_difference
     stm = stm_partitioning(
         stm,
-        db_partitioning=partitioning_cfg.db_partitioning,
         search_method=partitioning_cfg.search_method,
         cost_model=partitioning_cfg.cost_model,
         min_partition_size=partitioning_cfg.min_partition_size,
@@ -115,7 +82,6 @@ stm = stm.chunk({"time": -1, "space": cfg.ps_selection.output_chunks})
 if cfg.do_detect_outliers_stm:
     stm = detect_outliers_stm(
         stm,
-        db_outlier_detection=cfg.detect_outliers_stm.db_outlier_detection,
         window_size=cfg.detect_outliers_stm.window_size,
         n_sigma=cfg.detect_outliers_stm.n_sigma,
     )
@@ -124,5 +90,3 @@ if cfg.do_detect_outliers_stm:
 # Save
 stm.to_zarr(cfg.paths['stm_file'], mode='w')
 print("Done!")
-
-client.close()
