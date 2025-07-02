2300b68fa5dc0ed839142888c30ff8914ce5b5e4
SarahAlidoost <f.alidoost@esciencecenter.nl>
2025-07-02 15:54:43 +0200

    add functions for kriging

--- depsi/atmosphere.py
+++ depsi/atmosphere.py
@@ -7,10 +7,15 @@ signal.
 epoch.
 """
 
+from logging import getLogger
+
 import numpy as np
+import pykrige
 import xarray as xr
 from scipy import signal
 from scipy.ndimage import convolve1d
+
+logger = getLogger(__name__)
 
 
 def estimate_non_linear_deformation(
@@ -91,11 +96,145 @@ def estimate_non_linear_deformation(
     )
 
 
-def krige_in_space():
+def krige_per_single_time(
+        da: xr.DataArray,
+        grid: xr.Dataset | xr.DataArray | None = None,
+        method='universal',
+        **kwargs
+    ):
+    """Kriging in space for a single time step.
+
+    Make sure that coordinates 'x' and 'y' are present in the DataArray and they
+    are in metric units.
+    """
+    # Check that da.data shape is 2d
+    if len(da.data.shape) > 2:
+        raise ValueError("DataArray must be 2D with coordinates 'x' and 'y'.")
+
+    # Check if there x, y coords
+    if 'x' not in da.coords or 'y' not in da.coords:
+        raise ValueError("DataArray must have coordinates 'x' and 'y'.")
+
+    # Define default variogram parameters
+    # as implemented in Matlab version
+    variogram_model = kwargs.get('variogram_model', 'gaussian')
+    if variogram_model == 'gaussian':
+        default_variogram_parameters = {
+            'sill': 0.8 * da.var(),
+            'range': 1000.0,
+            'nugget': 0.2 * da.var()
+        }
+    else:
+        default_variogram_parameters = None
+
+    # Create a kriging instance
+    if method == 'universal':
+        kriging_obj = pykrige.uk.UniversalKriging(
+            da.coords['x'],
+            da.coords['y'],
+            da,
+            variogram_model=variogram_model,
+            variogram_parameters=kwargs.get('variogram_parameters', default_variogram_parameters),
+        )
+    else:
+        raise NotImplementedError(f"{method} is not implemented yet.")
+
+    if grid is None:
+        return kriging_obj
+
+    # Check if grid has x, y coords
+    if 'x' not in grid.coords or 'y' not in grid.coords:
+        raise ValueError("Grid must have coordinates 'x' and 'y'.")
+
+    # Calculates a kriged grid and the associated variance
+    # result has shape (M, N): M y coords and N x coords
+    return kriging_obj.execute(
+        'grid',
+        grid.coords['x'],  # shape (N,)
+        grid.coords['y'],  # shape (M,)
+        backend=kwargs.get('backend', 'vectorized'),
+    )
+
+
+def _create_grid(bbox, grid_size: int = 100):
+    """Create a grid based on the bounding box and grid size."""
+    if isinstance(bbox, list | tuple) or len(bbox) == 4:
+        x_min, y_min, x_max, y_max = bbox
+    else:
+        raise ValueError("Bounding box must be a list or tuple of four elements: [x_min, y_min, x_max, y_max].")
+
+    new_x = np.arange(x_min, x_max + grid_size, grid_size)
+    new_y = np.arange(y_min, y_max + grid_size, grid_size)
+    return xr.Dataset(coords={'x': new_x, 'y': new_y})
+
+
+def krige_in_space(
+    ps_atmosphere: xr.DataArray,
+    grid: xr.Dataset | None = None,
+    grid_size: int = 100,
+    method='universal',
+    **kwargs
+):
     """Kriging in space to estimate the atmosphere signal."""
+    # Check if ps_atmosphere has time dimension
+    if 'time' not in ps_atmosphere.dims:
+        raise ValueError(
+            "ps_atmosphere must have a 'time' dimension. "
+            "Otherwise, use `krige_per_single_time` function directly."
+        )
+
+    # Check if ps_atmosphere has 'x' and 'y' coordinates
+    if 'x' not in ps_atmosphere.coords or 'y' not in ps_atmosphere.coords:
+        raise ValueError("ps_atmosphere must have coordinates 'x' and 'y'.")
+
+    # Check if ps_atmosphere is chunked in time
+    if ps_atmosphere.chunks is not None and 'time' not in ps_atmosphere.chunks:
+        logger.info(
+            "It is better if `ps_atmosphere` is chunked in time. "
+            "Use `chunk` method to chunk it in time."
+        )
+
+    if grid is None:
+        bbox = [
+            ps_atmosphere["x"].min(),
+            ps_atmosphere["y"].min(),
+            ps_atmosphere["x"].max(),
+            ps_atmosphere["y"].max()
+        ]
+        grid = _create_grid(bbox, grid_size)
+
+    def apply_krige_per_single_time(data: np.ndarray):
+        """Apply kriging for a single time step."""
+        da = xr.DataArray(
+            data,
+            dims=['space'],
+            coords={'x': ('space', ps_atmosphere["x"].data), 'y': ('space', ps_atmosphere["y"].data)}
+        )
+
+        interpolated, sigmasq = krige_per_single_time(da, grid, method=method, **kwargs)
+        return interpolated.data, sigmasq.data
+
+    # setting output_sizes gives `FutureWarning`, but it is needed when
+    # ps_atmosphere is chunked, see
+    # https://docs.xarray.dev/en/stable/generated/xarray.apply_ufunc.html
+    interpolated, sigmasq = xr.apply_ufunc(
+        apply_krige_per_single_time,
+        ps_atmosphere,
+        input_core_dims=[['space']],
+        output_core_dims=[['y', 'x'], ['y', 'x']],
+        dask='parallelized',
+        vectorize=True,
+        output_dtypes=[ps_atmosphere.dtype, ps_atmosphere.dtype],
+        output_sizes={'y': len(grid["y"].data), 'x': len(grid["x"].data)}
+    )
+
+    return xr.Dataset({
+        'interpolated': interpolated,
+        'sigmasq': sigmasq
+    })
 
 
-def estimate_atmosphere_phase(stm):
+def estimate_atmosphere_phase(stm, grid=None):
     """Estimate the atmosphere phase.
 
     This function applies a temporal filter to extract the high-frequency
@@ -112,3 +251,10 @@ def estimate_atmosphere_phase(stm):
     stm["atmosphere_estimated"] = stm["d_phase"] - non_linear + stm["atmosphere_base"]
 
     # Step 2: Apply spatial kriging to estimate atmospheric phase per epoch
+    interpolated = krige_in_space(
+        ps_atmosphere=stm["atmosphere_estimated"],
+        grid=grid,
+        grid_size=100,
+    )
+
+    return interpolated
