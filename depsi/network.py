8ff83b6a904b35de6816b37c190e349c99f3649d
Ou Ku <o.ku@esciencecenter.nl>
2025-10-02 05:11:19 +0200

    rename function remove isolated points

--- depsi/network.py
+++ depsi/network.py
@@ -275,8 +275,8 @@ def arc_selection(
     return arcs_selected
 
 
-def remove_isolated_stm(stm: xr.Dataset, arcs: xr.Dataset) -> xr.Dataset:
-    """Remove isolated points from the STM."""
+def remove_isolated_points(stm: xr.Dataset, arcs: xr.Dataset) -> xr.Dataset:
+    """Remove isolated points from the network."""
     # Load source and target indices from arcs
     # these are 1d arrays so should fit in memory
     idx_source = arcs["source"].values
