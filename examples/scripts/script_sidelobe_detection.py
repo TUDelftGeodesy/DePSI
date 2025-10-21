efd3879b705cf7999c60b8866533d1121861e339
Simon van Diepen <s.vandiepen98@gmail.com>
2025-10-21 11:53:26 +0200

    Debugging example

--- examples/scripts/script_sidelobe_detection.py
+++ examples/scripts/script_sidelobe_detection.py
@@ -7,7 +7,12 @@ import xarray as xr
 from depsi.classification import detect_side_lobes
 
 # The original STM is output from the script script_ps_point_selection.py
-stm_original = "/Users/sanvandiepen/PycharmProjects/workingEnvironment2/test_zarr/nl_amsterdam_s1_dsc_t110_stm.zarr"
+stm_original = (
+    "/Users/sanvandiepen/PycharmProjects/workingEnvironment2/test_zarr/nl_amsterdam_s1_dsc_t110_stm_base.zarr"
+)
+stm_v2 = (
+    "/Users/sanvandiepen/PycharmProjects/workingEnvironment2/test_zarr/nl_amsterdam_s1_dsc_t110_base_corr.zarr"
+)
 stm_save_path = (
     "/Users/sanvandiepen/PycharmProjects/workingEnvironment2/test_zarr/nl_amsterdam_s1_dsc_t110_stm_nosl.zarr"
 )
@@ -17,6 +22,8 @@ min_correlation = 0.90
 
 # first read the STM
 stm = xr.open_zarr(stm_original)
+raster = xr.open_zarr(stm_v2)
+import pdb; pdb.set_trace()
 
 side_lobes_array, _ = detect_side_lobes(stm, max_pixel_dist, min_correlation)
 
