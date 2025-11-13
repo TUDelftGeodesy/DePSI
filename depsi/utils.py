0d37ea902e7934cb2a6256deb8a3b92cfb4ee9d9
Simon-van-Diepen <44979564+Simon-van-Diepen@users.noreply.github.com>
2025-11-13 17:42:33 +0100

    Update depsi/utils.py
    Co-authored-by: Ou Ku <o.ku@esciencecenter.nl>

--- depsi/utils.py
+++ depsi/utils.py
@@ -148,17 +148,6 @@ def get_distance(
     -------
         The distance between the two points in meters.
     """
-    # TODO(tvl) More complex distance functions can be implemented here.
-    #
-    # For example, network generation gives better results for square Euclidean distances.
-    # Non-square coordinate systems (like non-square image coordinates) may distort the circle properties of Delaunay
-    # networks into ellipses.
-    # Non-Euclidean coordinate systems (like angular lat-lon systems) may distort these same properties depending on
-    # the distance to a pole.
-    #
-    # Coordinate system transformations may be done on the STM before generating the network.
-    # However, there may be cases where it is impossible or undesirable to transform the point coordinates in the STM.
-    # In such a case, coordinates may be transformed inside this function.
     if mode == "euclidean":
         return math.dist(s, t)
     elif mode == "geographic":
