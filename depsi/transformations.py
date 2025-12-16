adc2b4144491a29e3b104e1ca5b9b832b8d54c83
Simon-van-Diepen <44979564+Simon-van-Diepen@users.noreply.github.com>
2025-12-16 15:31:13 +0100

    Initial docs depsi/transformations.py
    Co-authored-by: Ou Ku <o.ku@esciencecenter.nl>

--- depsi/transformations.py
+++ depsi/transformations.py
@@ -1,3 +1,16 @@
+"""Geocoding module.
+Functions of transformation between WGS84 coordinates (lat, lon, ellipsoid height) and Radar Coordinates (azimuth, range, ellipsoid height).
+
+When transfering from Rardar to WGS84, the following steps are performed:
+1. radar_to_xyz (calling radar_to_time internally)
+2. xyz_to_latlonh
+
+When transfering from WGS84 to Radar, the following steps are performed
+1. latlonh_to_xyz
+2. xyz_to_time
+3. time_to_radar
+"""
+
 import collections
 import datetime
 import logging
