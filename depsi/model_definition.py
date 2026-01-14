41a00d82d4df72725e3c5522f4d40f342bc74740
Ou Ku <o.ku@esciencecenter.nl>
2026-01-14 16:51:14 +0100

    docstrings

--- depsi/model_definition.py
+++ depsi/model_definition.py
@@ -1,15 +1,197 @@
 """Definition of models used in deformation analysis."""
 
 import numpy as np
+import xarray as xr
+
+# Look-up dictionary from model names to output model parameter names
+# Only models defined here can be used in the model estimation function
+# Note that the order of output parameters will follow the order in this list
+MODEL_NAMES_PARAMS = {
+    "linear": ["pnt_offset", "pnt_velocity"],  # offset + linear velocity
+    "height": ["pnt_height"],  # point height
+}
 
 
-def a_linear(time):
+def estimate_model_params(
+    stm: xr.Dataset,
+    models: list[str] = None,
+    key_unw_phase: str = "unw_phase",
+    key_h2ph: str = "h2ph",
+    key_time: str = "time",
+    key_temperature: str = "temperature",
+    key_cr2ph: str = "cr2ph",
+    wavelength: float | None = None,
+) -> xr.Dataset:
+    """Estimate model parameters for all points in the Space-Time Matrix.
+
+    The model estimation is performed based on unwrapped phase values and specified model components.
+    The function supports multiple models, which can be combined to form a comprehensive model for each point.
+    By default, we use a model which combines a linear model (offset + velocity) and a height model.
+    The estimated model parameters are added to the input STM as new variables.
+
+    Parameters
+    ----------
+    stm : xr.Dataset
+        Space-time dataset
+    models : list[str], optional
+        List of model names to be used for estimation.
+        By default this argument is None, which means using the default models: ["linear", "height"].
+    key_unw_phase : str, optional
+        Key for unwrapped phase in the STM, by default "unw_phase"
+    key_h2ph : str, optional
+        Key for height-to-phase conversion factor in the STM, by default "h2ph"
+    key_time : str, optional
+        Key for time coordinate in the STM, by default "time"
+    key_temperature : str, optional
+        Key for temperature in the STM, by default "temperature"
+    key_cr2ph : str, optional
+        Key for conversion factor from temperature to phase in the STM, by default "cr2ph"
+    wavelength : float | None, optional
+        Wavelength used for phase conversion, by default None
+
+    Returns
+    -------
+    xr.Dataset
+        Dataset containing estimated model parameters for each point.
+        All estimated parameters are assumed to only have "space" dimension.
+        In case a model has multiple parameters, they will be splitted into separate variables.
+    """
+    # Default model components
+    if models is None:
+        models = [
+            "linear",
+            "height",
+        ]
+    else:
+        # Reorder models according to MODEL_NAMES_PARAMS
+        ordered_models = []
+        for model_name in MODEL_NAMES_PARAMS.keys():
+            if model_name in models:
+                ordered_models.append(model_name)
+        models = ordered_models
+
+    # Get wavelength from metadata if not provided
+    if wavelength is None:
+        if "wavelength" in stm.attrs:
+            wavelength = stm.attrs["wavelength"]
+        else:
+            raise ValueError("Wavelength must be provided either as an argument or in the dataset attributes.")
+    m2ph = -4 * np.pi / wavelength
+
+    # Check models should be in the keys of MODEL_NAMES_PARAMS
+    for model in models:
+        if model not in MODEL_NAMES_PARAMS.keys():
+            raise NotImplementedError(
+                f"Model '{model}' is not supported. Available models are: {list(MODEL_NAMES_PARAMS.keys())}"
+            )
+
+    # Prepare space-time arguments
+    # These are args with (space, time) dimensions, and "time" as the core dimension
+    # It will be split into corresponding points of the unwrapped phase, each with (time,) dimension
+    st_args = []
+    st_args_keys = []  # keep track of keys for args
+    if "height" in models:
+        st_args.append(stm[key_h2ph])
+        st_args_keys.append("h2ph")
+
+    # Prepare keyword args
+    # These are arguments which will be broadcasted to each point
+    kwargs = {
+        "models": models,
+        "m2ph": m2ph,
+        "time": stm[key_time],  # Time coordinate is broadcasted to each point
+        "st_args_keys": st_args_keys,  # pass the keys for st_args to identify them in the function
+    }
+
+    # Organize output parameter names
+    param_names = []
+    for model in models:
+        param_names.extend(MODEL_NAMES_PARAMS[model])
+
+    # Apply model estimation for each point
+    params = xr.apply_ufunc(
+        _estimate_model_params_one_point,
+        stm[key_unw_phase],
+        *st_args,
+        input_core_dims=[["time"]] * (len(st_args) + 1),  # all args plus unw_phase have core dim "time"
+        output_core_dims=[[]] * len(param_names),  # per point per parameter, the output is a scalar
+        vectorize=True,
+        dask="parallelized",
+        output_dtypes=[float] * len(param_names),
+        kwargs=kwargs,
+    )
+
+    # Assign parameter names to the output dataset
+    stm_out = stm.copy()
+    for i, param_name in enumerate(param_names):
+        stm_out[param_name] = params[i]
+
+    return stm_out
+
+
+def _estimate_model_params_one_point(
+    unw_phase: np.ndarray,
+    *st_args,
+    models: list[str],
+    m2ph: float,
+    time: np.ndarray,
+    st_args_keys: list[str],
+) -> tuple:
+    """Estimate model parameters for a single point based on unwrapped phase values."""
+    # Loop though models and build A matrix
+    for model in models:
+        if model == "linear":
+            A_model = a_linear(time, m2ph)
+        elif model == "height":
+            # Get index of "h2ph" in st_args based on st_args_keys
+            h2ph_idx = st_args_keys.index("h2ph")
+            A_model = a_height(
+                st_args[h2ph_idx],
+                m2ph,
+            )
+
+        # Stack A matrices horizontally
+        if model == models[0]:
+            A = A_model
+        else:
+            A = np.hstack((A, A_model))
+
+    # Estimate model parameters using least squares
+    params, _, _, _ = np.linalg.lstsq(A, unw_phase, rcond=None)
+
+    return tuple(params)
+
+
+def a_height(h2ph, m2ph):
+    """Create the A matrix to estimate point height.
+
+    Parameters
+    ----------
+    h2ph : np.ndarray
+        Array of height-related values. Shape (n_epochs, ).
+    m2ph : float
+        Meter to phase conversion factor.
+
+    Returns
+    -------
+    np.ndarray
+        The A matrix, which is the reshaped height array multiplied by the given multiplier. Shape (n, 1).
+
+    """
+    A_h2ph = (np.reshape(h2ph, (len(h2ph), 1))) * m2ph
+
+    return A_h2ph
+
+
+def a_linear(time, m2ph):
     """Generate the A matrix for a linear model based on time.
 
     Parameters
     ----------
     time : np.ndarray
         Array of time values (in years). Shape (n, ).
+    m2ph : float
+        Meter to phase conversion factor.
 
     Returns
     -------
@@ -17,7 +199,7 @@ def a_linear(time):
         The A matrix, a 2-column matrix with ones in the first column and the time in the second column. Shape (n, 2).
     """
     A = np.ones((len(time), 2))
-    A[:, 1] = time
+    A[:, 1] = time * m2ph
 
     return A
 
@@ -74,27 +256,6 @@ def a_temperature(temp):
     A_temp = np.reshape(temp, (len(temp), 1))
 
     return A_temp
-
-
-def a_height(h2ph, m2ph):
-    """Create the A matrix to estimate point height.
-
-    Parameters
-    ----------
-    h2ph : np.ndarray
-        Array of height-related values. Shape (n, ).
-    m2ph : float
-        Multiplier for the height values, typically representing a scaling factor or coefficient.
-
-    Returns
-    -------
-    np.ndarray
-        The A matrix, which is the reshaped height array multiplied by the given multiplier. Shape (n, 1).
-
-    """
-    A_h2ph = (np.reshape(h2ph, (len(h2ph), 1))) * m2ph
-
-    return A_h2ph
 
 
 def a_cross_range(b_cr):
